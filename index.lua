20b84f8c62f2ccc07b2f5d2b5d918c1220df35a8
Allan Miljan <allan.miljan@tptlive.ee>
2025-11-24 12:45:44 +0200

    fixed jokers.lua, added file reading snippet from Hot-Potato

--- index.lua
+++ index.lua
@@ -11,11 +11,11 @@
 
 SMODS.ObjectType({
     key = "Burger",
-    defualt = "j_jimbo",
+    default = "j_reserved_parking", -- changed from 'defualt'
     cards = {},
     inject = function(self)
         SMODS.ObjectType.inject(self)
-        self:inject_card(G.P_CENTERS.j_jimbo)
+        self:inject_card(G.P_CENTERS.j_reserved_parking)
     end,
 })
 
@@ -28,6 +28,60 @@ SMODS.ObjectType({
     end,
 })
 
+-- FILE LOADING
+--#region File Loading
+local nativefs = NFS
+
+local path_len = string.len(SMODS.current_mod.path) + 1
+
+local function load_file_native(path)
+	if not path or path == "" then
+		error("No path was provided to load.")
+	end
+	local file_path = path
+	local file_content, err = NFS.read(file_path)
+	if not file_content then
+		return nil,
+			"Error reading file '" .. path .. "' for mod with ID '" .. SMODS.current_mod.id .. "': " .. err
+	end
+	local short_path = string.sub(path, path_len, path:len())
+	local chunk, err = load(file_content, "=[SMODS " .. SMODS.current_mod.id .. ' "' .. short_path .. '"]')
+	if not chunk then
+		return nil,
+			"Error processing file '" .. path .. "' for mod with ID '" .. SMODS.current_mod.id .. "': " .. err
+	end
+	return chunk
+end
+local blacklist = {
+	assets = true,
+	lovely = true,
+	[".github"] = true,
+	[".git"] = true,
+	["localization"] = true
+}
+local function load_files(path, dirs_only)
+	local info = nativefs.getDirectoryItemsInfo(path)
+	table.sort(info, function(a, b)
+		return a.name < b.name
+	end)
+	for _, v in ipairs(info) do
+		if v.type == "directory" and not blacklist[v.name] then
+			load_files(path .. "/" .. v.name)
+		elseif not dirs_only then
+			if string.find(v.name, ".lua") then -- no X.lua.txt files or whatever unless they are also lua files
+				local f, err = load_file_native(path .. "/" .. v.name)
+				if f then
+					f()
+				else
+					error("error in file " .. v.name .. ": " .. err)
+				end
+			end
+		end
+	end
+end
+local path = SMODS.current_mod.path
+
+--#endregion
 
 
 ----------------------------------------------------------
--- items/jokers.lua
+++ items/jokers.lua
@@ -11,16 +11,16 @@
 
 SMODS.Atlas{
     key = 'burgerking',
-    path = 'burgerking.png',
+    path = 'Burgerking.png',
     px = 71,
     py = 95,
 }
 
 SMODS.Joker{
-    key = 'burgerking'
-    loc_txt= {
-        name = 'Burgerking'õ
-        text = {"mm burger {C:mult}+4{} Mult"}
+    key = 'burgerking',
+    loc_txt = {
+        name = 'Burgerking',
+        text = {"mm burger {C:mult}+4{} Mult"},
     },
     atlas = 'burgerking',
     rarity = 1,
@@ -30,19 +30,19 @@ SMODS.Joker{
     unlocked = true,
     discovered = true,
     blueprint_compat = true,
-    eternal_compat false,
+    eternal_compat = false,
     perishable_compat = false,
 
     post = { x = 0, y = 0, },
-    config = { extra = {Mult = 4 }},
+    config = { extra = { mult = 4 } },
 
     loc_vars = function(self, info_queue, context)
-        return { vars = { center.ability.extra.mult }}
+        return { vars = { mult = self.config.extra.mult } }
     end,
     get_mods = function(self, card, context)
         if context.joker_main then
             return {
-                message = "+".. card.ability.extra.mult .. "Mult",
+                message = "+" .. card.ability.extra.mult .. "Mult",
                 mult_mod = card.ability.extra.mult,
             }
         end
