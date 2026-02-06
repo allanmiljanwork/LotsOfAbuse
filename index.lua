ab7e2a8d7d8df2dc79e6eb45d190dbcfdac59be4
Allan Miljan <allan.miljan@tptlive.ee>
2026-02-06 16:41:27 +0200

    Implement Chicot logic to kris card

--- index.lua
+++ index.lua
@@ -2,28 +2,28 @@
 --- MOD_NAME: LotsofAbuse
 --- MOD_ID: Lotsofabuse
 --- MOD_AUTHOR: Lotsofabuse
---- MOD_DESCRIPTION: 
+--- MOD_DESCRIPTION:
 --- PREFIX: lotsofabuse
 ----------------------------------------------------------
 ----------- MOD CODE -------------------------------------
 
 SMODS.ObjectType({
-    key = "Burger",
-    default = "j_reserved_parking", -- changed from 'defualt'
-    cards = {},
-    inject = function(self)
-        SMODS.ObjectType.inject(self)
-        self:inject_card(G.P_CENTERS.j_reserved_parking)
-    end,
+	key = "Burger",
+	default = "j_reserved_parking", -- changed from 'defualt'
+	cards = {},
+	inject = function(self)
+		SMODS.ObjectType.inject(self)
+		self:inject_card(G.P_CENTERS.j_reserved_parking)
+	end,
 })
 
 SMODS.ObjectType({
-    key = "Lotsofabuse",
-    default = "j_reserved_parking",
-    cards = {},
-    inject = function(self)
-        SMODS.ObjectType.inject(self)
-    end,
+	key = "Lotsofabuse",
+	default = "j_reserved_parking",
+	cards = {},
+	inject = function(self)
+		SMODS.ObjectType.inject(self)
+	end,
 })
 
 
--- items/kris.lua
+++ items/kris.lua
@@ -2,7 +2,6 @@
 
 SMODS.Atlas {
     key = 'kris',
-
     path = 'Krischan.png',
     px = 71,
     py = 95
@@ -10,16 +9,13 @@ SMODS.Atlas {
 
 SMODS.Joker {
     key = 'kris',
-
     loc_txt = {
         name = 'Kris-chan',
-        text = {"{C:legendary,E:1}Blocks all negative effects{}"}
+        text = { "{C:legendary,E:1}Blocks all negative effects{}" }
     },
-
     atlas = 'kris',
     rarity = 4,
     cost = 20,
-
     pools = {
         ["Lotsofabuse"] = true
     },
@@ -28,17 +24,36 @@ SMODS.Joker {
     blueprint_compat = false,
     eternal_compat = false,
     perishable_compat = false,
-
     pos = {
         x = 0,
         y = 0
     },
-
     loc_vars = function(self, info_queue, card)
         return {
             vars = {}
         }
-    end
+    end,
 
-    -- Implement event or G.GAME function to block all base game negative effects 
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                message = "",
+                colour = G.C.RARITY.Legendary
+            }
+        end
+
+        if context.setting_blind and not self.getting_sliced then
+            if context.blind.boss and not context.blueprint then
+                -- Sourcecode of Chicot
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        G.GAME.blind:disable()
+                        play_sound('timpani')
+                        delay(0.4)
+                        return true
+                    end
+                }))
+            end
+        end
+    end
 }
